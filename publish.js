2a7339949f4c8e5c47282f16fd8b8b1223056251
LaksCastro <lakscastro336@gmail.com>
2020-09-10 22:09:46 +0000

    remove unnused files

--- publish.js
+++ publish.js
@@ -2,8 +2,7 @@ const gh = require("gh-pages");
 
 gh.publish(
   "./dist",
-  { 
-    repo: 'https://' + process.env.GH_TOKEN + '@github.com/LaksCastro/lakscastro.github.io',
+  {
     branch: "master",
   },
   function (e) {
